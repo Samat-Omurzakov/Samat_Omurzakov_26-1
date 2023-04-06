404dd01ca489c56896108852c56c767ebfbdae0d
Samat-Omurzakov <samat-omurzakov@mail.ru>
2023-04-06 16:33:59 +0600

    hw7

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from products.models import Product, Review
 from products.forms import ProductCreateForm, ReviewCreateForm
+from products.constants import PAGINATION_LIMIT
 
 
 def main_view(request):
@@ -11,9 +12,17 @@ def main_view(request):
 def products_view(request):
     if request.method == 'GET':
         products = Product.objects.all()
+        search = request.GET.get('search')
+        page = int(request.GET.get('page', 1))
+        if search:
+            products = products.filter(name__icontains=search) | products.filter(description__icontains=search)
+        max_page = products.__len__() / PAGINATION_LIMIT
+        max_page = round(max_page) + 1 if round(max_page) < max_page else round(max_page)
+        products = products[PAGINATION_LIMIT * (page - 1): PAGINATION_LIMIT * page]
         context = {
             'products': products,
-            'user': request.user
+            'user': request.user,
+            'pages': range(1, max_page + 1)
         }
         return render(request, 'products/products.html', context=context)
 
