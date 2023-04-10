cccf858ad74f86c973774e426388ac269a390ed7
Samat-Omurzakov <samat-omurzakov@mail.ru>
2023-04-10 17:21:31 +0600

    hw8

--- OnlineStore/urls.py
+++ OnlineStore/urls.py
@@ -15,17 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from products.views import main_view, products_view, product_detail_view, product_create_view
+from products.views import MainPageCBV, ProductCBV, ProductDetailCBV, ProductCreateCBV
 from django.conf.urls.static import static
 from django.conf import settings
 from users.views import register_view, login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main_view),
-    path('products/', products_view),
-    path('products/<int:id>/', product_detail_view),
-    path('products/create/', product_create_view),
+    path('', MainPageCBV.as_view()),
+    path('products/', ProductCBV.as_view()),
+    path('products/<int:id>/', ProductDetailCBV.as_view()),
+    path('products/create/', ProductCreateCBV.as_view()),
     path('users/register/', register_view),
     path('users/login/', login_view),
     path('users/logout/', logout_view)
--- products/views.py
+++ products/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import render, redirect
 from products.models import Product, Review
 from products.forms import ProductCreateForm, ReviewCreateForm
 from products.constants import PAGINATION_LIMIT
+from django.views.generic import ListView, CreateView, DetailView
 
 
-def main_view(request):
-    if request.method == 'GET':
-        return render(request, 'layouts/index.html')
+class MainPageCBV(ListView):
+    model = Product
+    template_name = 'layouts/index.html'
 
 
-def products_view(request):
-    if request.method == 'GET':
-        products = Product.objects.all()
+class ProductCBV(ListView):
+    model = Product
+    template_name = 'products/products.html'
+
+    def get(self, request, **kwargs):
+        products = self.get_queryset()
         search = request.GET.get('search')
         page = int(request.GET.get('page', 1))
         if search:
@@ -24,7 +28,7 @@ def products_view(request):
             'user': request.user,
             'pages': range(1, max_page + 1)
         }
-        return render(request, 'products/products.html', context=context)
+        return render(request, self.template_name, context=context)
 
 
 def product_detail_view(request, id):
@@ -41,38 +45,85 @@ def product_detail_view(request, id):
         product = Product.objects.get(id=id)
         form = ReviewCreateForm(data=request.POST)
 
-    if form.is_valid():
-        Review.objects.create(
-            text=form.cleaned_data.get('text'),
-            product_id=id
-        )
+        if form.is_valid():
+            Review.objects.create(
+                text=form.cleaned_data.get('text'),
+                product_id=id
+            )
 
-    context = {
-        'product': product,
-        'reviews': product.review_set.all(),
-        'form': form
-    }
-    return render(request, 'products/detail.html', context=context)
-
-
-def product_create_view(request):
-    if request.method == 'GET':
         context = {
-            'form': ProductCreateForm
+            'product': product,
+            'reviews': product.review_set.all(),
+            'form': form
         }
-        return render(request, 'products/create.html', context=context)
+        return render(request, 'products/detail.html', context=context)
 
-    if request.method == 'POST':
-        form = ProductCreateForm(request.POST, request.FILES)
+
+class ProductDetailCBV(DetailView, CreateView):
+    model = Product
+    template_name = 'products/detail.html'
+    form_class = ReviewCreateForm
+    pk_url_kwarg = 'id'
+
+    # def get_context_data(self, *, object_list=None, **kwargs):
+    #     return {
+    #         'product': self.get_object(),
+    #         'reviews': Review.objects.filter(product=self.get_object()),
+    #         'form': self.form_class,
+    #         'user': request.user
+    #     }
+
+    def get(self, request, **kwargs):
+        context = {
+            'product': self.get_object(),
+            'id': self.get_object().id,
+            'reviews': Review.objects.filter(product=self.get_object()),
+            'form': self.form_class,
+            'user': request.user
+        }
+        return render(request, self.template_name, context=context)
+
+    def post(self, request, *args, **kwargs):
+        # product = Product.objects.get(id=id)
+        form = self.form_class(data=request.POST)
 
         if form.is_valid():
-            Product.objects.create(
+            Review.objects.create(
+                text=form.cleaned_data.get('text'),
+                product_id=id
+            )
+
+        context = {
+            'product': self.get_object(),
+            'id': self.get_object().id,
+            'reviews': Review.objects.filter(product=self.get_object()),
+            'form': form
+        }
+        return render(request, self.template_name, context=context)
+
+
+class ProductCreateCBV(ListView, CreateView):
+    model = Product
+    template_name = 'products/create.html'
+    form_class = ProductCreateForm
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        return {
+            'form': kwargs['form'] if kwargs.get('form') else self.form_class
+        }
+
+    def get(self, requset, **kwargs):
+        return render(requset, self.template_name, context=self.get_context_data())
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+
+        if form.is_valid():
+            self.model.objects.create(
                 name=form.cleaned_data.get('name'),
                 description=form.cleaned_data.get('description'),
                 price=form.cleaned_data.get('price'),
                 image=form.cleaned_data.get('image')
             )
             return redirect('/products/')
-        return render(request, 'products/create.html', context={
-            'form': form
-        })
+        return render(request, self.template_name, context=self.get_context_data(form=form))
